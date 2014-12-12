7c1a89c98e20fe4aa92935fef4ce3ef7d682dc7c
Stefano Teodorani <teopost@gmail.com>
2014-12-12 14:42:43 +0100

    +deleted

--- output/csharp/IDWS_Notificatore.bat
+++
@@ -1,8 +0,0 @@
-for /f "tokens=*" %%a in ('"ver"') do set VER=%%a
-if %VER:~-9,3%==6.2 goto WIN8
-if %VER:~-9,3%==6.3 goto WIN8
-start "WebServer" "%cd%\IDWS_Notificatore.exe" "%cd%" 1308 "/Notificatore/" "Notificatore.aspx" 30 "iexplore.exe" ""
-goto END
-:WIN8
-start "WebServer" "%cd%\IDWS8_Notificatore.exe" "%cd%" 1308 "/Notificatore/" "Notificatore.aspx" 30 "iexplore.exe" ""
-:END
--- output/csws/IDWS_NotificatoreWS.bat
+++
@@ -1,8 +0,0 @@
-for /f "tokens=*" %%a in ('"ver"') do set VER=%%a
-if %VER:~-9,3%==6.2 goto WIN8
-if %VER:~-9,3%==6.3 goto WIN8
-start "WebServer" "%cd%\IDWS_NotificatoreWS.exe" "%cd%" 3998 "/NotificatoreWS/" "NotificatoreWS.asmx" 30 "iexplore.exe" ""
-goto END
-:WIN8
-start "WebServer" "%cd%\IDWS8_NotificatoreWS.exe" "%cd%" 3998 "/NotificatoreWS/" "NotificatoreWS.asmx" 30 "iexplore.exe" ""
-:END
