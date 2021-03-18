db40d29b0bb84bedf711f1a9691b0f5f81421862
Daniele Nucci <d.nucci@apexnet.it>
2021-03-18 10:33:30 +0100

    Req. 7892 Modifiche per chiamata nuova API che effettua l'invio della notifica con il protocollo http2 (solo per iOSAndroid rimane uguale)

---
+++ custom/App_code/PushNotificationWrapperHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+using com.progamma;
+
+using PushNotificationWrapper;
+
+/// <summary>
+/// Summary description for PushNotificationWrapperHelper
+/// </summary>
+public static class PushNotificationWrapperHelper
+{
+    // Events
+
+    //this even raised when a notification is successfully sent
+    public static void NotificationSucceeded(INotification notification)
+    {
+        
+        MyWebEntryPoint mw = new MyWebEntryPoint(null);
+		IosNotification iosNotif = (IosNotification)notification;
+        PushNotificationWrapperEvents.ONNotificationSucceeded(iosNotif, mw, mw.getIMDB());
+        mw.CloseAllDBConnections();
+        
+    }
+
+    // //this is raised when a notification is failed due to some reason
+    public static void NotificationFailed(INotification notification, String error)
+    {
+       
+        MyWebEntryPoint mw = new MyWebEntryPoint(null);
+		com.progamma.IDVariant ErrorMessage = new com.progamma.IDVariant();
+        ErrorMessage.set(new com.progamma.IDVariant(error));
+		
+		IosNotification IosNotif = (IosNotification)notification;
+		
+        PushNotificationWrapperEvents.ONNotificationFailed(IosNotif, ErrorMessage, mw, mw.getIMDB());
+		
+        mw.CloseAllDBConnections();
+    }
+
+}
